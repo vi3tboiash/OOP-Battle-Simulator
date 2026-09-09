450fa45d9fdebac217bb80d5d003db499b310039
vi3tboiash <vi3tboiash@gmail.com>
2026-09-09 08:31:30 -0500

    Update game.py
    customed arena introduction

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ def main():
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("Grubenstein")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print("But no hero has answered the call... yet.")
