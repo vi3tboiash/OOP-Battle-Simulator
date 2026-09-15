0122303a2025d1ed422cc8f2d21315a0d9ee55ec
vi3tboiash <vi3tboiash@gmail.com>
2026-09-15 08:18:17 -0500

    updated equipment(again)

--- equipment.py
+++ equipment.py
@@ -1,8 +1,8 @@
 class Equipment:
-    def __init__(self, name, Type, power):
-            self.name = name
-            self.type = Type
-            self.power = power
+     def __init__(self, name, Type, power):
+          self.name = name
+          self.type = Type
+          self.power = power
 
      def add(self, entity):
           if self.type != "potion":
@@ -15,20 +15,24 @@ class Equipment:
                entity.equipList[slot]=self.name
                if removed.type=="armor":
                     entity.armor= entity.armor-removed.power
-                elif removed.type=="atk":
-                     entity.attack = entity.attack-removed.power*2
-            
-        if self.type=="potion":
-             print(f"added {self.power*5} to {entity.name}'s health")
-        elif self.type=="def":
-             entity.armor = entity.armor + self.power
-             print(f"added {self.power} to {entity.name}'s defense")
-        elif self.type=="atk":
-             entity.attack = entity.attack+self.power*2
-             print(f"added {self.power*2} to {entity.name}'s power")
-        else:
-             entity.maxHP = entity.maxHP+10
-             entity.armor = entity.armor+self.power*0.25
-             entity.attack = entity.attack + self.power
-             print(f"added 10 max health, {self.power*0.25} defense, and {self.power} attack power to {entity.name}")
-        
+               elif removed.type=="atk":
+                    entity.attack = entity.attack-removed.power*2
+               else:
+                    entity.maxHP = entity.maxHP-10
+                    entity.armor = entity.armor-removed.power*0.25
+                    entity.attack = entity.attack - removed.power
+
+          if self.type=="potion":
+               print(f"added {self.power*5} to {entity.name}'s health")
+          elif self.type=="def":
+               entity.armor = entity.armor + self.power
+               print(f"added {self.power} to {entity.name}'s defense")
+          elif self.type=="atk":
+               entity.attack = entity.attack+self.power*2
+               print(f"added {self.power*2} to {entity.name}'s power")
+          else:
+               entity.maxHP = entity.maxHP+10
+               entity.armor = entity.armor+self.power*0.25
+               entity.attack = entity.attack + self.power
+               print(f"added 10 max health, {self.power*0.25} defense, and {self.power} attack power to {entity.name}")
+
